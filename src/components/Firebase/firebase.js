c085c177aa76f26e879336912ffa2e9105509ddf
viettran1010 <48788462+viettran1010@users.noreply.github.com>
2021-02-03 20:49:04 +0700

    Update firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -18,13 +18,11 @@ const config = {
       this.auth = app.auth();
     }
 
-    doCreateUserWithEmailAndPassword = (email, password) => {
-        this.auth.createUserWithEmailAndPassword(email, password)
-    }
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password)    
 
-    doSignInWithEmailAndPassword = (email, password) => [
-        this.auth.signInWithEmailAndPassword(email, password)
-    ]
+    doSignInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password)    
 
     doSignOut = ()=> this.auth.signOut();
 
